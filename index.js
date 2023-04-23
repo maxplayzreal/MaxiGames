697f7117b04c97e16421f6ee3f755d574e6bccee
maxplayzreal <126032950+maxplayzreal@users.noreply.github.com>
2023-04-23 16:07:02 -0400

    Beta v1.0.1
    a

---
+++ index.js
@@ -0,0 +1,12 @@
+var password = 'EpikwebsiteofStormandmax'
+
+function passCheck() {
+    if(document.getElementById('pass1').value != password) {
+        alert('not correct password you dinglenut, try again lol');
+        return false;
+    }
+
+    if(document.getElementById('pass1').value = password) {
+       alert('Correct! Press "OK" to proceed')
+    }
+}
---
+++ randomMessageLoad.js
@@ -0,0 +1,73 @@
+let val = 1
+val = Math.floor(Math.random() * 16) + 1
+
+switch(val) {
+    case 1:
+        console.log('Loading the fun...')
+        $('#message').html('Loading the fun...')
+        break
+    case 2:
+        console.log('Brewing some games...')
+        $('#message').html('Brewing some games...')
+        break
+    case 3:
+        console.log('Do cats meow?')
+        $('#message').html('Do cats meow?')
+        break
+    case 4:
+        console.log('UwU...')
+        $('#message').html('UwU...')
+        break
+    case 5:
+        console.log('I play too much Bloxorz...')
+        $('#message').html('I play too much Bloxorz...')
+        break
+    case 6:
+        console.log('Free Showbucks')
+        $('#message').html('Free Showbucks')
+        break
+    case 7:
+        console.log('Dylan was here...')
+        $('#message').html('Dylan was here...')
+        break
+    case 8:
+        console.log('Comes with a free toy!')
+        $('#message').html('Comes with a free toy!')
+        break
+    case 9:
+        console.log('Sub to MaxPlayz and Storm')
+        $('#message').html('Sub to MaxPlayz and Storm')
+        break
+    case 10:
+        console.log('Also try hollow knight!')
+        $('#message').html('Also try hollow knight!')
+        break
+    case 11:
+        console.log('This is a certified hood classic')
+        $('#message').html('This is a certified hood classic')
+        break
+    case 12:
+        console.log('According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway')
+        $('#message').html('According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway')
+        break
+    case 13:
+        console.log('amogus...')
+        $('#message').html('amogus...')
+        break
+    case 14:
+        console.log('Its Raining Tacos!')
+        $('#message').html('Its Raining Tacos!')
+        break
+    case 15:
+        console.log('Coding is hard >:(')
+        $('#message').html('Coding is hard >:(')
+        break
+    case 16:
+        console.log('BIRD BIRD WORD. A B-B-B WORD WORD')
+        $('#message').html('BIRD BIRD WORD. A B-B-B WORD WORD')
+        break
+    default:
+        console.log('Hello!')
+        $('#message').html('Hello!')
+        break
+}
